c60019592558f6589e57bd79e4dadd7e2061a156
Max Larsson <student@macadmicBook166.fios-router.home>
2020-01-12 20:44:03 -0500

    Added some documentation and added the README file to Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,17 @@
 use std::io::{stdin, stdout, Write};
 
-pub fn input(input_string: &str) -> String {
-    print!("{}", input_string);
+/// Returns a String with what the user typed in response to the prompt.
+///
+/// # Arguments
+///
+/// * `prompt` - A &str that is printed to the console as a prompt for the user.
+///
+/// # Remarks
+///
+/// This is a convenience function that just shortens the amount of code that is
+/// necessary to recieve user input in response to a prompt, such as a question.
+pub fn input(prompt: &str) -> String {
+    print!("{}", prompt);
     let mut input = String::new();
 
     stdout().flush().expect("Failed to flush stdout!");
